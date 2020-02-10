6382e9c4652f934b232ead384ac8dff9cd6ef2e6
Vhimabinduv <60208896+Vhimabinduv@users.noreply.github.com>
2020-02-10 03:36:36 -0800

    Add files via upload

--- WebConfig.java
+++ WebConfig.java
@@ -1,24 +1,37 @@
-package com.iiht.config;
+package com.vp.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.iiht"})
+@ComponentScan(basePackages= {"com.vp"})
 public class WebConfig extends WebMvcConfigurerAdapter{
 	
-   @Bean
-   public InternalResourceViewResolver viewResolver(){
-	   InternalResourceViewResolver InternalResourceViewResolver =
-			   new InternalResourceViewResolver();
-	   InternalResourceViewResolver.setViewClass(JstlView.class);
-	   InternalResourceViewResolver.setPrefix("/WEB-INF/jsp/");
-	   InternalResourceViewResolver.setSuffix(".jsp");
-	   return InternalResourceViewResolver;
-   }
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		
+		InternalResourceViewResolver viewResolver =
+				new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource =
+				new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
